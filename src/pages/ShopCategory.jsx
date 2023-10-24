86d09c4dbbcb9d21453fe94eda31932e8cb0ab34
barkat ullah <barkatullahkhan120136@gmail.com>
2023-10-25 02:24:26 +0500

    read me

--- src/pages/ShopCategory.jsx
+++ src/pages/ShopCategory.jsx
@@ -8,13 +8,14 @@ const ShopCategory = (props) => {
   const {all_product}= useContext(ShopContext);
   return (
     <div className='shopCategory'>
-    <img src={props.banner}/>
+    <img src={props.banner}  className='shop-categoryBanners'/>
     <div className='shop-category-index'>
-      <p><span>showing 1-12</span> Out of 36 products</p>
+      <p><span>Showing 1-12</span> Out of 36 Products</p>
     </div>
     <div className='shop-category-sort'>
       Sort by <img src={drop_down}/>
     </div>
+    <div className='img'>
     <div className='all-product'>
       { all_product &&  all_product.map((item,i)=>{
         if(props.category === item.category){
@@ -23,7 +24,7 @@ return <Items  key={i} id={item.id} name={item.name} image={item.image} new_pric
           return null
         }
       })}
-    </div>
+    </div></div>
 
     </div>
   )
